7e432cef51980260badd33ba59384a10fa3e4f33
muhammadilham730 <mi8190044@gmail.com>
2024-11-20 08:56:35 +0700

    crudilham1

--- views/form_pengguna.php
+++ views/form_pengguna.php
@@ -14,13 +14,13 @@
                     <label for="exampleInputPassword1">Password</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                   </div>
-                  <div class="form-group">
+                 <!-- <div class="form-group">
                     <label for="exampleInputFile">File input</label>
                     <div class="input-group">
                       <div class="custom-file">
                         <input type="file" class="custom-file-input" id="exampleInputFile">
                         <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
+                      </div>-->
                       <div class="input-group-append">
                         <span class="input-group-text">Upload</span>
                       </div>
