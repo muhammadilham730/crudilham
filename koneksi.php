268fc8b77ad933ae3673bf0010996918abb7bbc1
muhammadilham730 <mi8190044@gmail.com>
2024-11-20 08:50:52 +0700

    crudilham

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$server="localhost";
+$user_db="root";
+$pass_db="";
+$db_name="crudilham";
+$koneksi =mysqli_connect($server,$user_db,$pass_db,$db_name);
+
+if(mysqli_connect_error()){
+    echo "koneksi Gagal:".mysqli_connect_error();
+}
